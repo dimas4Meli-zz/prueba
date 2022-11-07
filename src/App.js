20452eeafbf0a728f2f225629b4b54d567466827
Dimas Antonio Mendoza Lozano <dimas.mendoza@mercadolibre.com.co>
2022-11-07 17:32:17 -0500

    Commit verificado 6

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Commit Verificado Version 5</p>
+        <p>Commit Verificado Version 6</p>
         <a
           className='App-link'
           href='https://reactjs.org'
